c39aec2d6147bee6d6e83e8f3c326bd9c79bf18b
Claudia Alvarez Bravo <claualbr@gamma07.aulas.gsyc.urjc.es>
2018-10-01 14:42:48 +0200

    clacoohija.py final

--- calcoohija.py
+++ calcoohija.py
@@ -2,46 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-class Calculadora():
-	def plus(op1, op2):
-		""" Function to sum the operands. Ops have to be ints """
-		return op1 + op2
+import calcoo
 
 
-	def minus(op1, op2):
-		""" Function to substract the operands """
-		return op1 - op2
-class CalculadoraHija(Calculadora):
-	def mult(op1, op2):
-		""" Function to multiply the operands. Ops have to be ints """
-		return op1 * op2
+class CalculadoraHija(calcoo.Calculadora):
+    def mult(op1, op2):
+        """ Function to multiply the operands. Ops have to be ints """
+        return op1 * op2
 
-
-	def div(op1, op2):
-		""" Function to split the operands """
-		return op1 / op2
+    def div(op1, op2):
+        """ Function to split the operands """
+        return op1 / op2
 
 
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	if sys.argv[2] == "suma":
-		result = CalculadoraHija.plus(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = CalculadoraHija.minus(operando1, operando2)
-	elif sys.argv[2] == "divide":
-		try:
-			result = CalculadoraHija.div(operando1, operando2)
-		except ZeroDivisionError:
-			sys.exit("Division by zero is not allowed")
-	elif sys.argv[2] == "multiplica":
-		result = CalculadoraHija.mult(operando1, operando2)
-	else:
-		sys.exit('Operación sólo puede ser sumar o restar.')
+    if sys.argv[2] == "suma":
+        result = CalculadoraHija.plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = CalculadoraHija.minus(operando1, operando2)
+    elif sys.argv[2] == "divide":
+        try:
+            result = CalculadoraHija.div(operando1, operando2)
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed")
+    elif sys.argv[2] == "multiplica":
+        result = CalculadoraHija.mult(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
 
-	print(result)
+    print(result)

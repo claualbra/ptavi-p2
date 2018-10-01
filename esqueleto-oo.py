e57b1e499466b76992533f7ea06e80e64a51992a
claualbra <32228581+claualbra@users.noreply.github.com>
2018-10-01 15:25:27 +0200

    Update esqueleto-oo.py

--- esqueleto-oo.py
+++ esqueleto-oo.py
@@ -3,14 +3,13 @@
 
 
 class Clase(ClaseMadre):
-  """Esto es un ejemplo de clase que hereda de ClaseMadre"""
+    """Esto es un ejemplo de clase que hereda de ClaseMadre"""
+    def __init__(self, valor):
+        """Esto es el método iniciliazador"""
+        self.atributo = valor
 
-  def __init__(self, valor):
-    "Esto es el método iniciliazador"
-    self.atributo = valor
 
-    
 if __name__ == "__main__":
-  objeto = Clase("pepe") # Creo un objeto de la clase Clase
-                         # y le paso el valor pepe para su
-                         # atributo en la inicialización
+    objeto = Clase("pepe") """Creo un objeto de la clase Clase
+                           y le paso el valor pepe para su
+                           atributo en la inicialización"""

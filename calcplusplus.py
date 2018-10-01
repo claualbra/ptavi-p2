833d2d5cdd3bd839be7f04d8aae4670f5db5381b
Claudia Alvarez Bravo <claualbr@gamma07.aulas.gsyc.urjc.es>
2018-10-01 14:46:51 +0200

    clacplusplus.py final

--- calcplusplus.py
+++ calcplusplus.py
@@ -3,54 +3,44 @@
 
 import sys
 import csv
-
-class Calculadora():
-	def plus(lista_int):
-		""" Function to sum the operands. Ops have to be ints """
-		return sum(lista_int)
-
-	def minus(lista_int):
-		""" Function to substract the operands """
-		resta = lista_int[0]
-		for i in lista_int[1:]:
-			resta -= i
-		return resta
-
-class CalculadoraHija(Calculadora):
-	def mult(lista_int):
-		""" Function to multiply the operands. Ops have to be ints """
-		multiplicacion = 1
-		for i in lista_int:
-			multiplicacion *= i
-		return multiplicacion
-
-	def div(lista_int):
-		""" Function to split the operands """
-		division = lista_int[0]
-		for i in lista_int[1:]:
-			division /= i
-		return division
+import calcoohija
 
 
 if __name__ == "__main__":
-	with open(sys.argv[1]) as csvarchivo:
-		fichero = csv.reader(csvarchivo)
-		for line in fichero:
-			lista_calc = line
-			lista_int = list(map(int, line[1:]))
+    micalc = calcoohija.CalculadoraHija
+    with open(sys.argv[1]) as csvarchivo:
+        fichero = csv.reader(csvarchivo)
+        for line in fichero:
+            try:
+                lista_calc = line
+                lista_int = list(map(int, line[1:]))
 
-			if lista_calc[0] == "suma":
-				result = CalculadoraHija.plus(lista_int)
-			elif lista_calc[0] == "resta":
-				result = CalculadoraHija.minus(lista_int)
-			elif lista_calc[0] == "multiplica":
-				result = CalculadoraHija.mult(lista_int)
-			elif lista_calc[0] == "divide":
-				try:
-					result = CalculadoraHija.div(lista_int)
-				except ZeroDivisionError:
-					result = "Division by zero is not allowed"
-			else:
-				result = 'Operación sólo puede ser sumar o restar.'
-		
-			print(result)
+                if lista_calc[0] == "suma":
+                    suma = 0
+                    for op2 in lista_int:
+                        suma = micalc.plus(suma, op2)
+                    result = suma
+                elif lista_calc[0] == "resta":
+                    resta = lista_int[0]
+                    for op2 in lista_int[1:]:
+                        resta = micalc.minus(resta, op2)
+                    result = resta
+                elif lista_calc[0] == "multiplica":
+                    multiplicacion = 1
+                    for op2 in lista_int:
+                        multiplicacion = micalc.mult(multiplicacion, op2)
+                    result = multiplicacion
+                elif lista_calc[0] == "divide":
+                    division = lista_int[0]
+                    try:
+                        for op2 in lista_int[1:]:
+                            division = micalc.div(division, op2)
+                        result = division
+                    except ZeroDivisionError:
+                        result = "Division by zero is not allowed"
+                else:
+                    result = 'Op. sólo puede ser suma, resta, mult. o divide.'
+            except ValueError:
+                result = "Error: Non numerical parameters"
+
+            print(result)

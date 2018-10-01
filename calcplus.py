a89205562c9a2791cd3434116fe23b0836ca0241
claualbra <32228581+claualbra@users.noreply.github.com>
2018-10-01 18:21:34 +0200

    Add files via upload

--- calcplus.py
+++ calcplus.py
@@ -13,32 +13,28 @@ if __name__ == "__main__":
             lista_calc = line.split(",")
             lista_int = list(map(int, lista_calc[1:]))
 
-            if lista_calc[0] == "suma":
-                suma = 0
-                for op2 in lista_int:
-                    suma = micalc.plus(suma, op2)
-                result = suma
-            elif lista_calc[0] == "resta":
-                resta = lista_int[0]
-                for op2 in lista_int[1:]:
-                    resta = micalc.minus(resta, op2)
-                result = resta
-            elif lista_calc[0] == "multiplica":
-                multiplicacion = 1
-                for op2 in lista_int:
-                    multiplicacion = micalc.mult(multiplicacion, op2)
-                result = multiplicacion
-            elif lista_calc[0] == "divide":
-                division = lista_int[0]
-                try:
-                    for op2 in lista_int[1:]:
-                        division = micalc.div(division, op2)
-                    result = division
-                except ZeroDivisionError:
-                    result = "Division by zero is not allowed"
-            else:
-                result = 'Op. sólo puede ser suma, resta, multiplica o divide.'
+            op1 = lista_int[0]
+            for op2 in lista_int[1:]:
+                if lista_calc[0] == "suma":
+                    op1 = micalc.plus(op1, op2)
+                    result = op1
+                elif lista_calc[0] == "resta":
+                    op1 = micalc.minus(op1, op2)
+                    result = op1
+                elif lista_calc[0] == "multiplica":
+                    op1 = micalc.mult(op1, op2)
+                    result = op1
+                elif lista_calc[0] == "divide":
+                    if op2 == 0:
+                        result = "Division by zero is not allowed"
+                        break
+                    else:
+                        op1 = micalc.div(op1, op2)
+                        result = op1
+                else:
+                    result = 'Op. sólo puede ser suma,resta,mult. o divide.'
         except ValueError:
             result = "Error: Non numerical parameters"
 
         print(result)
+    fd.close()

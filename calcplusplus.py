9c59462d305819e285d291d62802c9cd6641dec7
claualbra <32228581+claualbra@users.noreply.github.com>
2018-10-01 18:18:00 +0200

    Add files via upload

--- calcplusplus.py
+++ calcplusplus.py
@@ -2,44 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import csv
 import calcoohija
+import csv
 
 
 if __name__ == "__main__":
     micalc = calcoohija.CalculadoraHija
     with open(sys.argv[1]) as csvarchivo:
         fichero = csv.reader(csvarchivo)
+        operaciones = {"suma": micalc.plus,
+                       "resta": micalc.minus,
+                       "multiplica": micalc.mult,
+                       "divide": micalc.div}
         for line in fichero:
             try:
                 lista_calc = line
                 lista_int = list(map(int, line[1:]))
 
-                if lista_calc[0] == "suma":
-                    suma = 0
-                    for op2 in lista_int:
-                        suma = micalc.plus(suma, op2)
-                    result = suma
-                elif lista_calc[0] == "resta":
-                    resta = lista_int[0]
-                    for op2 in lista_int[1:]:
-                        resta = micalc.minus(resta, op2)
-                    result = resta
-                elif lista_calc[0] == "multiplica":
-                    multiplicacion = 1
-                    for op2 in lista_int:
-                        multiplicacion = micalc.mult(multiplicacion, op2)
-                    result = multiplicacion
-                elif lista_calc[0] == "divide":
-                    division = lista_int[0]
+                op1 = lista_int[0]
+                for op2 in lista_int[1:]:
                     try:
-                        for op2 in lista_int[1:]:
-                            division = micalc.div(division, op2)
-                        result = division
-                    except ZeroDivisionError:
-                        result = "Division by zero is not allowed"
-                else:
-                    result = 'Op. sólo puede ser suma, resta, mult. o divide.'
+                        if op2 == 0 and lista_calc[0] == "divide":
+                            result = "Division by zero is not allowed"
+                            break
+                        else:
+                            op1 = operaciones[lista_calc[0]](op1, op2)
+                            result = op1
+                    except KeyError:
+                        result = "Sólo puede ser suma,resta,mult. o divide."
             except ValueError:
                 result = "Error: Non numerical parameters"
 
